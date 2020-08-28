94978989beb739a7064b3b11e5cdc804c9241902
alexpeachey <alex.peachey@gmail.com>
2020-08-28 12:54:52 -0700

    Step 13

---
+++ config/releases.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :nook_book, NookBookWeb.Endpoint,
+  server: true,
+  http: [port: 4000],
+  url: [host: "localhost"],
+  secret_key_base: "3t5dqOhPDTZUCJu4HwDHWBNr6PYtX1Rgr/VBDgoqX80NZvWGkmD3YK/JhLpmHWu7"
